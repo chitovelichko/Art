2d59152b6d04989c632e0fd8c9f5a1df0ed0c358
chitovelichko <103333840+chitovelichko@users.noreply.github.com>
2022-04-10 12:32:08 +0300

    Add files via upload

---
+++ first = 10.py
@@ -0,0 +1,36 @@
+first  = 10 
+second = 30
+result = first + second
+print(result)
+#Cтворіть дві змінні та ввисти на єкран результати мат. взаемодій
+#first=10; second=30; 
+result = first/second 
+print(result)
+
+
+result = first*second
+print(result)
+
+result = first**second
+print(result)
+
+#Створити змінні та записати порівняння чисел з 1 завдання
+my_bool = True
+my_bool = False
+result  = 10 < 30
+print(result)
+
+result= 10 > 30
+print(result)
+
+result = 10 == 30
+print(result)
+
+result = 10 != 30
+print(result)
+
+#Створити змінні та скласти строки str1="Hello";str2=world
+str1 = "Hello"
+str2 =  "World"
+result = str1 + str2
+print(result)
