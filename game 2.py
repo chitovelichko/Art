cc4ab3decc94b0357cc2c898eabdb16da58187d9
chitovelichko <103333840+chitovelichko@users.noreply.github.com>
2022-05-16 16:47:42 +0300

    Add files via upload

---
+++ game 2.py
@@ -0,0 +1,81 @@
+from random import choice
+import datetime
+
+WIN_RULES = {
+'stone': ['scissors', 'lizard'],
+'paper': ['stone', 'spouk'],
+'scissors': ['paper', 'lizard'],
+'spouk': ['scissors', 'stone'],
+'lizard': ['spouk', 'paper']
+}
+
+# get data from user
+def get_user_choice():
+	while True:
+		user_choice = input('Enter your choice (stone paper scissors lizard spouk): ')
+		if user_choice not in WIN_RULES.keys():
+			print('Enter only: stone paper scissors')
+			continue
+		else:
+			return user_choice
+
+# get data from computer
+def get_computer_choice():
+	computer_choice = choice(list(WIN_RULES.keys()))
+	return computer_choice
+
+# define winner
+def get_winner(user_choice, computer_choice):
+	if user_choice == computer_choice:
+		msg = 'Draw'
+	elif computer_choice in WIN_RULES[user_choice]:
+		msg = 'User'
+	else:
+		msg = 'Computer'
+	return msg
+
+# make message
+def make_message(user_choice, computer_choice, winner):
+	msg = f'User choice is {user_choice}, PC choice is {computer_choice}. Winner is {winner}'
+	return msg
+
+# save result
+def save_results(res):
+	data = []
+	try:
+		with open('results.txt', 'r', encoding='utf-8') as f:
+			for line in f:
+				data.append(line)
+	except:
+		pass
+	with open('results.txt', 'w', encoding='utf-8') as f:
+		for d in data:
+			f.write(d)
+		today = datetime.datetime.today().strftime("%d-%B-%Y %H:%M:%S")
+		f.write(f"{len(data)+1} {today} Player - {res['player']}, AI - {res['ai']}, Winner - {res['winner']}\n")
+from lib2to3 import *
+
+def main():
+	for i in range(1, 4):
+		print(f'Try {i}')
+		user_choice = get_user_choice()
+		computer_choice = get_computer_choice()
+		winner = get_winner(user_choice, computer_choice)
+		msg = make_message(user_choice, computer_choice, winner)
+		print(msg)
+		save_results({'player': user_choice, 'ai': computer_choice, 'winner': winner})
+
+main()
+from lib import *
+
+def main():
+	for i in range(1, 4):
+		print(f'Try {i}')
+		user_choice = get_user_choice()
+		computer_choice = get_computer_choice()
+		winner = get_winner(user_choice, computer_choice)
+		msg = make_message(user_choice, computer_choice, winner)
+		print(msg)
+		save_results({'player': user_choice, 'ai': computer_choice, 'winner': winner})
+
+main()
